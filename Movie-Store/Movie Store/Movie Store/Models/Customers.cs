018baf1de9abe5b7817f6a2d927b8ba4fd6fb1f4
Student <Student@LEXLINK-HP016>
2022-11-01 14:46:33 +0100

    Customers

---
+++ Movie-Store/Movie Store/Movie Store/Models/Customers.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+#nullable disable
+
+namespace Movie_Store.Models
+{
+    public class Customers
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "First name")]
+        [StringLength(200, MinimumLength = 2)]
+
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        [StringLength(200, MinimumLength = 2)]
+
+        public string LastName { get; set; }
+
+        [Display(Name = "Billing adress")]
+        [StringLength(200, MinimumLength = 2)]
+        public string BillingAdress { get; set; }
+
+        [Display(Name = "Billing zip")]
+        [StringLength(100, MinimumLength = 2)]
+        public string BillingZip { get; set; }
+
+        [Display(Name = "Delivery adress")]
+        [StringLength(200, MinimumLength = 2)]
+        public string DeliveryAddress { get; set; }
+
+        [Display(Name = "Delivery zip")]
+        [StringLength(100, MinimumLength = 2)]
+        public string DeliveryZip { get; set; }
+
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        [StringLength(256)]
+        public string EmailAdress { get; set; }
+
+        [Display(Name = "Phone nr")]
+        [Phone]
+        [StringLength(100)]
+        public string PhoneNo { get; set; }
+
+
+
+    }
+}
