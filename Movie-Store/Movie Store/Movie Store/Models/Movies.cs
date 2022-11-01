58e779634b7be6933c42de560a4de6f88aa37382
Marcus Gustafsson <marcus979@live.se>
2022-11-01 15:05:38 +0100

    Movies and Controllers

---
+++ Movie-Store/Movie Store/Movie Store/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Store.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Price { get; set; }
+
+    }
+}
