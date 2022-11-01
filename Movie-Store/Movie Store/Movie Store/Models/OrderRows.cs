417330d36a9aa27949ed3f17a7e27891f89c8aa9
Jari1980 <116716604+Jari1980@users.noreply.github.com>
2022-11-01 14:33:08 +0100

    Add files via upload

---
+++ Movie-Store/Movie Store/Movie Store/Models/OrderRows.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Store.Models
+{
+    public class OrderRows
+    {
+        [Key]
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+        public double Price { get; set; }
+    }
+}
