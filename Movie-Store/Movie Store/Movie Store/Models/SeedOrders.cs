3ca6fa494476afdac57d34608caae605ee5455f7
Marcus Gustafsson <marcus979@live.se>
2022-11-03 14:25:43 +0100

    Fix

--- Movie-Store/Movie Store/Movie Store/Models/SeedOrders.cs
+++ Movie-Store/Movie Store/Movie Store/Models/SeedOrders.cs
@@ -19,28 +19,28 @@ namespace Movie_Store.Models
                 context.Orders.AddRange(
                 new Orders
                 {
-                OrderDate = DateTime.Parse("2002-1-16"),
+                OrderDate = DateTime.Parse("2002-1-16 12:45"),
                 CustomerId = 1
                 },
 
                 new Orders
                 {
-                    OrderDate = DateTime.Parse("1959-4-15"),
+                    OrderDate = DateTime.Parse("1959-4-15 13:00"),
                     CustomerId = 3
                 },
                 new Orders
                 {
-                    OrderDate = DateTime.Parse("1997-10-24"),
+                    OrderDate = DateTime.Parse("1997-10-24 18:00"),
                     CustomerId = 2
                 },
                 new Orders
                 {
-                    OrderDate = DateTime.Parse("2022-11-02"),
+                    OrderDate = DateTime.Parse("2022-11-02 23:30"),
                     CustomerId =  5
                 },
                 new Orders
                 {
-                    OrderDate = DateTime.Parse("2003-6-04"),
+                    OrderDate = DateTime.Parse("2003-6-04 07:07"),
                     CustomerId = 1
                 }
 
